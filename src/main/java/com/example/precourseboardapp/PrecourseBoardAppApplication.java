7aeb643c57b56d6d944c54e807c3ba2da28ed602
hayul93 <parksy785@gmail.com>
2023-07-31 20:02:48 +0900

    init commmit

---
+++ src/main/java/com/example/precourseboardapp/PrecourseBoardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.precourseboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecourseBoardAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrecourseBoardAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/precourseboardapp/PrecourseBoardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.precourseboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecourseBoardAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
