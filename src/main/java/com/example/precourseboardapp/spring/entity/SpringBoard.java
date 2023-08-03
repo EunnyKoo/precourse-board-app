8aaa3fd009d74450c65a8b30a8e02c6cbd074b20
hayul93 <parksy785@gmail.com>
2023-08-03 20:38:20 +0900

    spring commit

--- src/main/java/com/example/precourseboardapp/spring/entity/SpringBoard.java
+++ src/main/java/com/example/precourseboardapp/spring/entity/SpringBoard.java
@@ -2,6 +2,7 @@ package com.example.precourseboardapp.spring.entity;
 
 
 import com.example.precourseboardapp.global.entity.Timestamped;
+import com.example.precourseboardapp.spring.dto.SpringRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,4 +20,18 @@ public class SpringBoard extends Timestamped {
     private String password;
     @Column(length = 2000)
     private String contents;
+
+    public SpringBoard(SpringRequestDto springRequestDto) {
+        this.title = springRequestDto.getTitle();
+        this.author = springRequestDto.getAuthor();
+        this.password = springRequestDto.getPassword();
+        this.contents = springRequestDto.getContents();
+    }
+
+    public void update(SpringRequestDto springRequestDto) {
+        this.title = springRequestDto.getTitle();
+        this.author = springRequestDto.getAuthor();
+        this.password = springRequestDto.getPassword();
+        this.contents = springRequestDto.getContents();
+    }
 }
